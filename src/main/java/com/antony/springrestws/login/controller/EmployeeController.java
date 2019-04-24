19229ecca62412ab33c506965b58f6a68c6285e4
ma1 <ma1@nationwide.com>
2019-04-24 13:10:10 +0530

    test

--- src/main/java/com/antony/springrestws/login/controller/EmployeeController.java
+++ src/main/java/com/antony/springrestws/login/controller/EmployeeController.java
@@ -39,7 +39,7 @@ public class EmployeeController {
     @RequestMapping(value="/retrieveEmployee", method=RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public@ResponseBody Employee  retrieveEmployee(@RequestParam("empID") String empID){					
-    	LOG.info("Start:: 12355fffdfffs896EmployeeController --> retrieveEmployee()");    	
+    	LOG.info("Start:: ddd12355fffdfffs896EmployeeController --> retrieveEmployee()");    	
         emp = new Employee();       
         emp.setId(new Integer(empID));
         Employee obj=employeeService.retrieveEmployee(emp,emp.getId());
