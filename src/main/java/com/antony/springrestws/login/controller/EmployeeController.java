d2bb1c340e8c4b79a414b3337ce5de7f37d987a4
ma1 <ma1@nationwide.com>
2019-04-24 13:09:52 +0530

    Revert "test"
    This reverts commit d93d20edb412843d9be3afa10b52069bd7b8d73b.

--- src/main/java/com/antony/springrestws/login/controller/EmployeeController.java
+++ src/main/java/com/antony/springrestws/login/controller/EmployeeController.java
@@ -39,7 +39,7 @@ public class EmployeeController {
     @RequestMapping(value="/retrieveEmployee", method=RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public@ResponseBody Employee  retrieveEmployee(@RequestParam("empID") String empID){					
-    	LOG.info("Start:: test12355fffdfffs896EmployeeController --> retrieveEmployee()");    	
+    	LOG.info("Start:: 12355fffdfffs896EmployeeController --> retrieveEmployee()");    	
         emp = new Employee();       
         emp.setId(new Integer(empID));
         Employee obj=employeeService.retrieveEmployee(emp,emp.getId());
